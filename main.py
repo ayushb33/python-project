cf9bdf76715bbb90eecb6d675fa13e23f691bb6f
AYUSH BEHERA <34929509-ayushbehera777@users.noreply.replit.com>
2024-06-28 08:14:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("This is a Band Name Generator!!! WELCOME!!")
+city_name=input("What is the city you belong to?\n")
+pet_name=input("What is your pet's name?\n")
+band_name=city_name+" " +pet_name
+print("Your band name is : "+band_name)
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
